77265d1a0c5ba96c07737285b0bd4ce12913dd7f
Andrei Campigotto <andreicampigotto@gmail.com>
2022-08-24 15:48:18 -0300

    criado arquitetura

---
+++ app/src/main/java/com/example/testeandroidsicredi/model/Event.kt
@@ -0,0 +1,4 @@
+package com.example.testeandroidsicredi.model
+
+class Event {
+}
---
+++ app/src/main/java/com/example/testeandroidsicredi/viewModel/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.testeandroidsicredi.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
