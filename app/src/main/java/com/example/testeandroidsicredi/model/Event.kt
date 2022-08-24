7c304a65d3e7a64ebbb83296acc07359d7e33cfc
Andrei Campigotto <andreicampigotto@gmail.com>
2022-08-24 15:58:09 -0300

    criado model

--- app/src/main/java/com/example/testeandroidsicredi/model/Event.kt
+++ app/src/main/java/com/example/testeandroidsicredi/model/Event.kt
@@ -1,4 +1,32 @@
 package com.example.testeandroidsicredi.model
 
-class Event {
-}
+import com.google.gson.annotations.SerializedName
+
+data class Event (
+    @SerializedName("people")
+    val people :  List<People>,
+    @SerializedName("date")
+    val date: Long,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("image")
+    val image: String,
+    @SerializedName("longitude")
+    val longitude: Long,
+    @SerializedName("latitude")
+    val latitude: Long,
+    @SerializedName("price")
+    val price : Float,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("id")
+    val id: Long,
+)
+
+data class EventResponse(
+    val results: List<Event>
+)
+
+data class People(
+    val id: Long
+)
